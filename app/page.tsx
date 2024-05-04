e6db482e9fbad407b70deefbe45545a4bf2f61f7
Guilherme Cardoso <guilhermecardoso.info@gmail.com>
2024-05-04 09:32:01 -0300

    feat: add header component and menu button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "./_components/Header";
+
 export default function Home() {
   return (
-    <div className="mx-5 bg-red-500 py-5">
-      <h1>Hello tailwind</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
