0ec4f87fd6822bd917ca03453d7f412534db41b0
Guilherme Cardoso <guilhermecardoso.info@gmail.com>
2024-05-04 10:07:22 -0300

    feat: add category item component

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from '@prisma/client'
+/* eslint-disable no-unused-vars */
+import { PrismaClient } from "@prisma/client";
 
 declare global {
   var cachedPrisma: PrismaClient;
 }
 
 let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global.cachedPrisma) {
